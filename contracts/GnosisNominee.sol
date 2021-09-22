d22c77aa99d0b04f8ce7ab510bf9c319e8690c02
Bhargav Aparoksham <aparokshambhargav@gmail.com>
2021-09-22 23:30:25 +0530

    nominee contract

--- contracts/GnosisNominee.sol
+++ contracts/GnosisNominee.sol
@@ -6,6 +6,11 @@ import "@gnosis.pm/zodiac/contracts/core/Module.sol";
 
 contract GnosisNominee is Module {
 
+    // Safe -> Owner -> Nominee
+    mapping(address => mapping (address => address)) public nominee;
+    // Safe -> Owner -> takeOverTime
+    mapping(address => mapping (address => uint256)) public takeOverTime;
+
     event NomineeModuleSetup(address indexed initiator, address indexed avatar);
 
 
@@ -23,8 +28,7 @@ contract GnosisNominee is Module {
         bytes memory initParams = abi.encode(
             _owner,
             _avatar,
-            _target,
-            _nominee
+            _target
         );
         setUp(initParams);
     }
@@ -33,21 +37,37 @@ contract GnosisNominee is Module {
         (
             address _owner,
             address _avatar,
-            address _target,
-            address _nominee
+            address _target
         ) = abi.decode(
             initParams,
-            (address, address, address, address)
+            (address, address, address)
         );
         __Ownable_init();
         require(_avatar != address(0), "Avatar can not be zero address");
         avatar = _avatar;
         require(_target != address(0), "Target can not be zero address");
         target = _target;
+        //nominee = _nominee;
 
         transferOwnership(_owner);
 
         emit NomineeModuleSetup(msg.sender, _avatar);
+    }
+
+
+    function nominate() public {
+
+        require( msg.sender = nominee, "Only a nominee can nominate")
+        // swapOwner(address prevOwner, address oldOwner, address newOwner)
+        //bytes memory data = abi.encodeWithSignature("transfer(address,uint256)", to, amount);
+        //require(safe.execTransactionFromModule(token, 0, data, Enum.Operation.Call), "Could not execute token transfer");
+
+        bytes memory data = abi.encodeWithSignature("swapOwner(address,address,address)", prevOwner, oldOwner, nominee);
+
+        require(
+            exec(avatar, 0, data, Enum.Operation.Call),
+            "Error in swapping owner"
+        );
     }
 
 }
