23f0e7abab863a0a9c7c03627c5c9b8f90001efc
Bhargav Aparoksham <bhargavaparoksham@Bhargavs-MacBook-Air.local>
2021-10-04 23:29:12 +0530

    minor updates

--- contracts/IGnosisSafe.sol
+++ contracts/IGnosisSafe.sol
@@ -5,11 +5,19 @@ pragma solidity ^0.8.6;
 
 interface IGnosisSafe {
 
+	// Swap Owner
+
 	function swapOwner(address prevOwner, address oldOwner, address newOwner) external;
+
+	// Is Owner of the safe
 
 	function isOwner(address owner) external view returns (bool);
 
+	// Get all owner addresses
+
 	function getOwners() external view returns (address[] memory);
+
+	// Get Threshold
 
     function getThreshold() external view returns (uint256);
 
